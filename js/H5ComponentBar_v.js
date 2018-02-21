238cfdc9eed33c4127345036e3ed26ed0f9e12d8
Airing <gx-deng@163.com>
2018-02-21 12:04:24 +0800

    style: add gitignore & update style

--- js/H5ComponentBar_v.js
+++ js/H5ComponentBar_v.js
@@ -1,19 +1,19 @@
 /* 垂直柱图组件对象 */
 
-var H5ComponentBar_v =function ( name, cfg ) {
+var H5ComponentBar_v = function (name, cfg) {
 
-  var component =  new H5ComponentBar( name ,cfg );
+  var component = new H5ComponentBar(name, cfg);
 
-  var width = ( 100 / cfg.data.length ) >> 0 ;
-  component.find('.line').width( width + '%');
-  
-  $.each( component.find('.rate') ,function(){
-      var w = $(this).css('width');
-      $(this).height(w).width('');
+  var width = ( 100 / cfg.data.length ) >> 0;
+  component.find('.line').width(width + '%');
+
+  $.each(component.find('.rate'), function () {
+    var w = $(this).css('width');
+    $(this).height(w).width('');
   });
 
-  $.each( component.find('.per'),function(){
-      $(this).appendTo( $(this).prev() ) ;
+  $.each(component.find('.per'), function () {
+    $(this).appendTo($(this).prev());
   })
 
   return component;
